4a0b0d81143fa11dc2a367975cc4436cbda5a46c
Bezerra <sabrina.bezerra@capgemini.com>
2023-11-17 20:33:04 -0300

    componente Row

---
+++ clone-netflix/src/App.js
@@ -0,0 +1,23 @@
+import "./App.css";
+import Row from "./components/Row";
+import categories from "./api";
+
+function App() {
+  return (
+    <div className="App">
+      {categories.map((category) => {
+        return (
+          <Row
+            key={category.name}
+            title={category.title}
+            path={category.path}
+            isLarge={category.isLarge}
+          />
+        );
+      })}
+    </div>
+  );
+}
+
+export default App;
+
